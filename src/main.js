51d70ab32cef9a050aabef6b910b7652ba39eaaa
nongqi <2460822214@qq.com>
2022-08-30 16:08:05 +0800

    [commit]引入字体图标

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+//字体图标
+import '@/assets/iconfont/iconfont.css'
+import '@/assets/iconfont/iconfont.js'
+//样式表
+import '@/assets/scss/index.scss'
 
 createApp(App).mount('#app')
